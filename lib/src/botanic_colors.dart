519537eb0e0b5f7fbc3c49552ec709f01c2b50ef
limadlp <dlplima@hotmail.com>
2025-03-10 23:11:05 -0300

    initial commit

---
+++ lib/src/botanic_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class BotanicColors {
+  static const Color primary = Color(0xFF693E2F);
+  static const Color secondary = Color(0xFFA87C5F);
+  static const Color neutral = Color(0xFFD3B9A3);
+  static const Color highlight = Color(0xFF63A375);
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+}
